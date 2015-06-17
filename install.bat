4dcbd0173c86b97918102c154fed2095b2a1c7ec
maximehelaine <mhelainedu91@live.fr>
2015-06-17 18:52:03 +0200

    Commit des choses bizarres

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
 rem take path to GraphicEngine
 
-setx Lib_GraphicEngine "C:\Users\Alexandre\Documents\GitHub\GraphicEngine\GraphicEngine\GraphicEngine"
+setx Lib_GraphicEngine "D:\Documents\GitHub\GraphicEngine\GraphicEngine\GraphicEngine"
